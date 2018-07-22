d7e372f8acdfd76767c07e34bbbbe7a0d29ffe39
Konstantin Kovalev <findias@bk.ru>
2018-07-22 11:41:23 +0300

    Insert new dir Codesignal and new first work in this dir

---
+++ codesignal/checkpalindrome.py
@@ -0,0 +1,15 @@
+
+def checkPalindrome(inputString):
+
+    InvertedString = inputString[::-1]
+    MiddleLine = len(inputString) // 2
+    LongFirstPart = inputString[0:MiddleLine:]
+    LongSecondPart = InvertedString[0:MiddleLine:]
+    print(len(inputString))
+    print(InvertedString)
+    print(MiddleLine)
+    print(LongFirstPart, LongSecondPart)
+    return print(LongFirstPart == LongSecondPart)
+
+
+checkPalindrome('hlbeeykoqqqqokyeeblh')
