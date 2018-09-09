d0fe8e3cf1e89beba2cc68027348056aaa441619
Konstantin Kovalev <findias@bk.ru>
2018-09-09 12:26:24 +0300

    New lessons 09

---
+++ codesignal/ElementsProduct.py
@@ -0,0 +1,8 @@
+
+def adjacentElementsProduct(inputArray):
+
+
+    return print(ElementSort)
+
+
+adjacentElementsProduct([3, 6, -2, -5, 7, 3])
--- codesignal/checkpalindrome.py
+++ codesignal/checkpalindrome.py
@@ -1,9 +1,9 @@
-
 def checkPalindrome(inputString):
 
     InvertedString = inputString[::-1]
     MiddleLine = len(inputString) // 2
-    LongFirstPart = inputString[0:MiddleLine:]
+    LongFirstPart = inputString[0:MiddleLine:
+    #LongSecondPart = inputString[MiddleLine::-1]
     LongSecondPart = InvertedString[0:MiddleLine:]
     print(len(inputString))
     print(InvertedString)
